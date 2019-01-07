75e4e748306beb915e8d2c4410399dd2a46fafb4
chrismenning <chrismenning@gmail.com>
2019-01-07 15:46:41 -0600

    test

--- StegoCrypto/Classes/BitmapEncoder.cs
+++ StegoCrypto/Classes/BitmapEncoder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,38 +13,55 @@ namespace StegoCrypto
     {
         // the private fields.
         private readonly Bitmap theBitmap;
-        private byte[] rgbValues;
+        private Color pixelColor;
+        private Color sanitizedColor;
         private BitArray OnesAndZeros;
-        BackgroundWorker bgWorker = new BackgroundWorker();
-        PleaseWait pwForm;
-        int h;
-        int w;
+        private int newA;
+        private int newR;
+        private int newG;
+        private int newB;
 
-        // The constructor.
+        // The constructor
         public BitmapEncoder(Bitmap rawBitmap)
         {
             this.theBitmap = rawBitmap;
-
-            InitializeBackgroundWorker();
         }
 
-        private void InitializeBackgroundWorker()
+        // The main method that returns an encoded bitmap.
+        public Bitmap EncodedBitmap(byte[] file, byte[] IV)
         {
-            bgWorker.DoWork +=
-                new DoWorkEventHandler(bgWorker_DoWork);
-            bgWorker.RunWorkerCompleted +=
-                new RunWorkerCompletedEventHandler(
-            bgWorker_RunWorkerCompleted);
-            bgWorker.ProgressChanged +=
-                new ProgressChangedEventHandler(
-            bgWorker_ProgressChanged);
+            PleaseWait pwForm = new PleaseWait();
 
-            bgWorker.WorkerReportsProgress = true;
-        }
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, this.theBitmap.Width, this.theBitmap.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                this.theBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                this.theBitmap.PixelFormat);
 
-        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
-        {
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * this.theBitmap.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            // Declare a counter.
             int counter = 0;
+            int h = this.theBitmap.Height;
+            int w = this.theBitmap.Width;
+
+            // Prepend IV onto file and convert them both to a BitArray.
+            OnesAndZeros = GetOnesAndZeros(IV, file);
+
+            // Update the progress bar.
+            pwForm.progress.Maximum = h;
+            pwForm.Show();
+            pwForm.Refresh();
+
+            // Loop through every row and every column of pixels in the original image.
             for (int row = 0; row < h; row++)
             {
                 // Only set a new pixel value until the message length runs out. 
@@ -66,53 +82,16 @@ namespace StegoCrypto
                             }
                         }
                     }
-                    bgWorker.ReportProgress(row / h);
+                    pwForm.progress.Invoke(pwForm.myDelegate);
+                    pwForm.progress.Value = row;
+                }
+                else
+                {
+                    pwForm.progress.Value = h;
                 }
             }
-        }
 
-        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-
-        }
-
-        private void bgWorker_ProgressChanged(object sender,
-            ProgressChangedEventArgs e)
-        {
-            pwForm.progress.Increment(e.ProgressPercentage);
-        }
-
-        // The main method that returns an encoded bitmap.
-        public Bitmap EncodedBitmap(byte[] file, byte[] IV)
-        {
-            // Lock the bitmap's bits.  
-            Rectangle rect = new Rectangle(0, 0, this.theBitmap.Width, this.theBitmap.Height);
-            System.Drawing.Imaging.BitmapData bmpData =
-                this.theBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                this.theBitmap.PixelFormat);
-
-            // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
-
-            // Declare an array to hold the bytes of the bitmap.
-            int bytes = Math.Abs(bmpData.Stride) * this.theBitmap.Height;
-            rgbValues = new byte[bytes];
-
-            // Copy the RGB values into the array.
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            // cache the bitmaps height and width.
-
-            h = this.theBitmap.Height;
-            w = this.theBitmap.Width;
-
-            // Prepend IV onto file and convert them both to a BitArray.
-            OnesAndZeros = GetOnesAndZeros(IV, file);
-
-            // Loop through every row and every column of pixels in the original image.
-            var t = new Thread(() => BeginWorkerAndProgress());
-            t.Start();
-            t.Join();
+            pwForm.Close();
 
             if (OnesAndZeros.Length / 8 > (w * h / 2))
                 Console.WriteLine("******************************* \n MESSAGE TRUNCATED WHILE WRITING TO BITMAP!!!");
@@ -123,16 +102,6 @@ namespace StegoCrypto
             // Unlock the bits.
             this.theBitmap.UnlockBits(bmpData);
             return this.theBitmap;
-        }
-
-        private void BeginWorkerAndProgress()
-        {
-            // Update the progress bar.
-            pwForm = new PleaseWait();
-            pwForm.progress.Maximum = h;
-            pwForm.Show();
-            pwForm.Refresh();
-            bgWorker.RunWorkerAsync();
         }
 
         // Prepend IV onto File as BitArray
