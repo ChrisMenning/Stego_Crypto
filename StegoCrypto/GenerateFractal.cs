65a6f85ca90a30747de23fd3df25b4f0fa1bbe7e
ChrisMenning <chrismenning@gmail.com>
2019-01-10 21:18:41 -0600

    updated readme

--- StegoCrypto/GenerateFractal.cs
+++ StegoCrypto/GenerateFractal.cs
@@ -225,89 +225,89 @@ namespace StegoCrypto
             Console.WriteLine("Thread Complete from " + startIndex + " to " + stopIndex);
         }
 
-           // HSV to RGB by Chris Hulbert http://www.splinter.com.au/converting-hsv-to-rgb-colour-using-c/
-           private void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
-           {
-               double H = h;
-               while (H < 0) { H += 360; };
-               while (H >= 360) { H -= 360; };
-               double R, G, B;
-               if (V <= 0)
-               { R = G = B = 0; }
-               else if (S <= 0)
-               {
-                   R = G = B = V;
-               }
-               else
-               {
-                   double hf = H / 60.0;
-                   int i = (int)Math.Floor(hf);
-                   double f = hf - i;
-                   double pv = V * (1 - S);
-                   double qv = V * (1 - S * f);
-                   double tv = V * (1 - S * (1 - f));
-                   switch (i)
-                   {
+        // HSV to RGB by Chris Hulbert http://www.splinter.com.au/converting-hsv-to-rgb-colour-using-c/
+        private void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
+        {
+            double H = h;
+            while (H < 0) { H += 360; };
+            while (H >= 360) { H -= 360; };
+            double R, G, B;
+            if (V <= 0)
+            { R = G = B = 0; }
+            else if (S <= 0)
+            {
+                R = G = B = V;
+            }
+            else
+            {
+                double hf = H / 60.0;
+                int i = (int)Math.Floor(hf);
+                double f = hf - i;
+                double pv = V * (1 - S);
+                double qv = V * (1 - S * f);
+                double tv = V * (1 - S * (1 - f));
+                switch (i)
+                {
         
-                       // Red is the dominant color
-                       case 0:
-                           R = V;
-                           G = tv;
-                           B = pv;
-                           break;
+                    // Red is the dominant color
+                    case 0:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
         
-                       // Green is the dominant color
-                       case 1:
-                           R = qv;
-                           G = V;
-                           B = pv;
-                           break;
-                       case 2:
-                           R = pv;
-                           G = V;
-                           B = tv;
-                           break;
+                    // Green is the dominant color
+                    case 1:
+                        R = qv;
+                        G = V;
+                        B = pv;
+                        break;
+                    case 2:
+                        R = pv;
+                        G = V;
+                        B = tv;
+                        break;
         
-                       // Blue is the dominant color
-                       case 3:
-                           R = pv;
-                           G = qv;
-                           B = V;
-                           break;
-                       case 4:
-                           R = tv;
-                           G = pv;
-                           B = V;
-                           break;
+                    // Blue is the dominant color
+                    case 3:
+                        R = pv;
+                        G = qv;
+                        B = V;
+                        break;
+                    case 4:
+                        R = tv;
+                        G = pv;
+                        B = V;
+                        break;
         
-                       // Red is the dominant color
-                       case 5:
-                           R = V;
-                           G = pv;
-                           B = qv;
-                           break;
+                    // Red is the dominant color
+                    case 5:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
         
-                       // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
-                       case 6:
-                           R = V;
-                           G = tv;
-                           B = pv;
-                           break;
-                       case -1:
-                           R = V;
-                           G = pv;
-                           B = qv;
-                           break;
+                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+                    case 6:
+                        R = V;
+                        G = tv;
+                        B = pv;
+                        break;
+                    case -1:
+                        R = V;
+                        G = pv;
+                        B = qv;
+                        break;
         
-                       default:
-                           R = G = B = V; // Just pretend its black/white
-                           break;
-                   }
-               }
-               r = (int)R * 10;
-               g = (int)G * 10;
-               b = (int)B * 10;
-           }
+                    default:
+                        R = G = B = V; // Just pretend its black/white
+                        break;
+                }
+            }
+            r = (int)R * 10;
+            g = (int)G * 10;
+            b = (int)B * 10;
+        }
 
         // https://stackoverflow.com/users/12971/greg
         public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
